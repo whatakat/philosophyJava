b9d9dbb489199504c8760bcdf3be10e2110828c6
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-11 23:35:07 +0300

    Add class Car

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/synchronizing/machineAssembly/Car.java
@@ -0,0 +1,18 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.synchronizing.machineAssembly;
+
+public class Car {
+    private final int id;
+    private boolean
+    engine = false, driveTrain = false, wheels = false;
+    public Car(int idn){id=idn;}
+    public Car(){id = -1;}
+    public synchronized int getId(){return id;}
+    public synchronized void addEngine(){engine = true;}
+    public synchronized void addDriveTrain(){driveTrain = true;}
+    public synchronized void addWheels(){wheels = true;}
+    public synchronized String toString(){
+        return "Car "+id+" ["+" engine: "+engine
+                +" driveTrain: "+driveTrain
+                +" wheels: "+wheels+" ]";
+    }
+}
