24db3f89b2bcde4a5d30771886de2dafaaff9093
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-11 23:36:48 +0300

    Add class CarQueue

---
+++ app/src/main/java/com/bignerdranch/testphilosophyjava/homecontrol/parallelExecution/synchronizing/machineAssembly/CarQueue.java
@@ -0,0 +1,7 @@
+package com.bignerdranch.testphilosophyjava.homecontrol.parallelExecution.synchronizing.machineAssembly;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class CarQueue extends LinkedBlockingQueue {
+
+}
